899c229b588c74cef7447fbd1acbffd5dd8aa48f
mattjburrows <mattjburrows@gmail.com>
2016-07-30 18:16:58 +0100

    Adding in checks for default values in the volume component

--- test/components/Volume.js
+++ test/components/Volume.js
@@ -8,13 +8,25 @@ import sinon from 'sinon';
 import Volume from '../../app/components/Volume.jsx';
 
 describe('<Volume />', () => {
+  it('renders the volume control with the default values', () => {
+    const value = 5;
+    const wrapper = shallow(
+      <Volume value={value} onChange={() => {}} />
+    );
+    const input = wrapper.find('input');
+
+    assert.equal(input.prop('min'), 0);
+    assert.equal(input.prop('max'), 100);
+    assert.equal(input.prop('step'), 5);
+  });
+
   it('renders the volume control with the correct value', () => {
     const value = 5;
     const wrapper = shallow(
       <Volume value={value} onChange={() => {}} />
     );
 
-    assert.equal(wrapper.props().value, 5);
+    assert.equal(wrapper.find('input').prop('value'), 5);
   });
 
   it('calls the onChange function', () => {
