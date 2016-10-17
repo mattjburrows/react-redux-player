df1256a838ab1e1870339d6a90c0153cca6369d0
Luke Barton <luke@lukebarton.co.uk>
2016-10-17 23:17:33 +0100

    Suggestions
    Stateless components (just a function, no life-cycle, pure) are a little faster.
    Unsure why you were doing the calculation of the position?

--- app/components/Progress.jsx
+++ app/components/Progress.jsx
@@ -1,31 +1,14 @@
 'use strict';
 
-import React, { Component } from 'react';
+import React from 'react';
 
-function workOutProgressValue(duration, currentTime) {
-  const difference = (duration - currentTime);
-  const remainingTime = (duration - difference);
-  const progressPercentage = (remainingTime / duration) * 100;
-
-  return progressPercentage;
-}
-
-class Progress extends Component {
-  render() {
-    const progress = workOutProgressValue(this.props.duration, this.props.currentTime);
-
-    return (<progress value={progress} max={this.props.max}></progress>);
-  }
-}
-
-Progress.defaultProps = {
-  max: 100
-};
+const Progress = (props) => (
+  <progress value={this.props.currentTime} max={this.props.duration} />
+)
 
 Progress.propTypes = {
-  duration: React.PropTypes.number,
-  currentTime: React.PropTypes.number,
-  max: React.PropTypes.number
+  duration: React.PropTypes.number.isRequired,
+  currentTime: React.PropTypes.number.isRequired
 };
 
 export default Progress;
